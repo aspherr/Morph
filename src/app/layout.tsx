b818a6c31e53ec1566ba01c90bf01adb027ed129
aspherr <huzaifa.syedd@gmail.com>
2025-09-22 18:17:30 +0100

    feat: added theme-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import { cn } from "@/lib/utils"; 
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={cn("min-h-screen flex flex-col bg-background text-foreground", geistSans.className, geistMono.className)}>
+
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bottom-0 left-0 mt-4 w-full flex items-center justify-center text-xs text-foreground">
+    <footer className="bottom-0 left-0 mt-4 w-full flex items-center justify-center font-sans text-sm text-foreground opacity-50">
         Morph © 2025
     </footer>
   )
