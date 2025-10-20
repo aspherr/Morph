f24104c2e6da94ade0cddcd76e41e402d18cb5d1
aspherr <huzaifa.syedd@gmail.com>
2025-10-20 16:08:45 +0100

    chore: updated styling for status component

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -1,5 +1,3 @@
-import { CircleCheck, CircleAlert } from "lucide-react"
-
 type StatusProps = {
   isReady: boolean;
 };
@@ -8,14 +6,12 @@ const Status = ({isReady}: StatusProps) => {
   return (
     <>
     {isReady ? (
-        <span className="flex items-center gap-1 px-2 py-1 text-[10px] bg-emerald-600 text-emerald-200 rounded-md ml-2 antialiased">
-            <CircleCheck strokeWidth={1.75} width={14} height={14} />
-            <span className="font-semibold">Done</span>
+        <span className="flex items-center gap-1 px-2 py-1 text-[10px] bg-emerald-600 text-emerald-200 rounded ml-2 antialiased">
+            <span className="font-semibold">DONE</span>
         </span>
     ) : (
-        <span className="flex items-center gap-1 px-2 py-1 text-[10px] bg-rose-600 text-rose-200 rounded-md ml-2 antialiased">
-            <CircleAlert strokeWidth={1.75} width={14} height={14} />
-            <span className="font-semibold">Failed</span>
+        <span className="flex items-center gap-1 px-2 py-1 text-[10px] bg-rose-600 text-rose-200 rounded ml-2 antialiased">
+            <span className="font-semibold">FAILED</span>
         </span>
     )}
     </>
