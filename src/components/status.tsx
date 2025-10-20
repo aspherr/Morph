87eb74b2a20cb1795d7540af805f0b7dfd21ffbb
aspherr <huzaifa.syedd@gmail.com>
2025-10-20 19:34:02 +0100

    chore: updated status text color

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -6,11 +6,11 @@ const Status = ({isReady}: StatusProps) => {
   return (
     <>
     {isReady ? (
-        <span className="flex items-center gap-1 px-2 py-1 text-[10px] bg-emerald-600 text-emerald-200 rounded ml-2 antialiased">
+        <span className="flex items-center gap-1 px-2 py-1 text-[10px] bg-emerald-600 text-white rounded ml-2 antialiased">
             <span className="font-semibold">DONE</span>
         </span>
     ) : (
-        <span className="flex items-center gap-1 px-2 py-1 text-[10px] bg-rose-600 text-rose-200 rounded ml-2 antialiased">
+        <span className="flex items-center gap-1 px-2 py-1 text-[10px] bg-rose-600 text-white rounded ml-2 antialiased">
             <span className="font-semibold">FAILED</span>
         </span>
     )}
